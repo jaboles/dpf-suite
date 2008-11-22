5275c3ad43fd6241f0e450b8bfa293c69105086f
jb <jb@d6262863-905e-8745-84b8-bfcf939036b6>
2008-11-22 01:48:16 +0000

    git-svn-id: https://jbpc/svn/dpf-suite@2 d6262863-905e-8745-84b8-bfcf939036b6

--- MonitorOn.bat
+++ MonitorOn.bat
@@ -2,4 +2,8 @@
 c:
 cd \pixsuite
 start /wait gmouse mousemove.gms
+ping 127.0.0.1 -n 3 > NUL
+start /wait gmouse mousemove.gms
+ping 127.0.0.1 -n 3 > NUL
+start /wait gmouse mousemove.gms
 wizmo quiet blank
--- StartManager.bat
+++ StartManager.bat
@@ -1,19 +1,27 @@
 @echo off
-start wizmo quiet play=welcome.wav
+start wizmo quiet play=macstartup.wav
 banner "Welcome!"
 
 echo Starting scheduler...
 start /min "Scheduler" "C:\Program Files\American Systems\EZ Scheduler\EZScheduler.exe"
 echo Done!
 
-echo Waiting 40 seconds for network to connect...
-:: wait 40 seconds
-ping 127.0.0.1 -n 40 > NUL
+echo Waiting for network to connect...
+:: wait for network to connect
+::ping 127.0.0.1 -n 15 > NUL
+:retryping
+ping 192.168.0.11 -n 2 | find "TTL=" > NUL
+echo Network Errorlevel: %errorlevel%
+if errorlevel 1 goto retryping
 echo Done!
 
 :: connect to the downstairs photo server
 echo Starting server connection...
-net use p: \\192.168.0.5\Photos blahblahblah /PERSISTENT:NO > NUL
+:retryserverconnection
+ping 127.0.0.1 -n 6 > NUL
+net use p: \\192.168.0.11\Photos yessyess /PERSISTENT:NO > NUL
+echo File-share Errorlevel: %errorlevel%, retrying...
+if errorlevel 1 goto retryserverconnection
 echo Done!
 
 echo Starting server connection...
